d76791a5dfe8cc0901cd7d5af667e1e68d02f3a1
manatsu-n <manatsu.nakamura@kard.co.jp>
2025-06-09 15:55:36 +0900

    コード修正

--- src/logic/CreateQuestion.ts
+++ src/logic/CreateQuestion.ts
@@ -15,45 +15,35 @@ export function usePuzzleGenerator() {
 
       const randomIndex = Math.floor(Math.random() * filteredPuzzles.length);
       const selectedPuzzle = filteredPuzzles[randomIndex];
-
-      let rawCreate = JSON.parse(selectedPuzzle.puzzle);
-      let rawAnswer = JSON.parse(selectedPuzzle.solution);
+      const {rawCreate, rawAnswer} = calcRaw(selectedPuzzle.puzzle, selectedPuzzle.solution);
       
-      
-      if (Math.random()<0.5){
-        rawCreate.reverse();
-        rawAnswer.reverse();
-      }
       createStore.setCreate([...rawCreate]);
       createStore.setAnswer([...rawAnswer]);
-
-      for (let i = 0; i < 10; i++) {
-        const a = Math.floor(9 * Math.random()) + 1;
-        const b = Math.floor(9 * Math.random()) + 1;
-        rawCreate = rawCreate.map((row: number[]) =>
-          row.map(v => {
-            if (v === a) return b;
-            if (v === b) return a;
-              return v;
-          })
-        );
-
-        rawAnswer = rawAnswer.map((row: number[]) =>
-          row.map(v => {
-            if (v === a) return b;
-            if (v === b) return a;
-            return v;
-          })
-        );
-      }
-
-
-      createStore.setCreate([...rawCreate]);
-      createStore.setAnswer([...rawAnswer]);
-
       levelStore.level=null
-
     },
     { immediate: true }
   );
+}
+
+function calcRaw(puzzleStr: string, solutionStr: string) {
+  let rawCreate = JSON.parse(puzzleStr);
+  let rawAnswer = JSON.parse(solutionStr);
+
+  if (Math.random() < 0.5) {
+    rawCreate.reverse();
+    rawAnswer.reverse();
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const a = Math.floor(9 * Math.random()) + 1;
+    const b = Math.floor(9 * Math.random()) + 1;
+    rawCreate = rawCreate.map((row: number[]) =>
+      row.map(v => (v === a ? b : v === b ? a : v))
+    );
+    rawAnswer = rawAnswer.map((row: number[]) =>
+      row.map(v => (v === a ? b : v === b ? a : v))
+    );
+  }
+
+  return { rawCreate, rawAnswer };
 }
--- src/logic/PuzzleQuestion.vue
+++ src/logic/PuzzleQuestion.vue
@@ -1,122 +1,122 @@
 <script setup lang="ts">
-    import { ref, computed, watch, onMounted } from "vue"
-    import { useCreateStore, useNumberStore, usekesigomuStore, usehintoStore, useWrongStore, useClearflagStore, useMemoStore } from "../stores/level.ts";
-    import { usePuzzleGenerator } from "./CreateQuestion";
-    const puzzleDiv = ref<HTMLDivElement | null>(null);
+import { ref, computed, watch, onMounted } from "vue"
+import { useCreateStore, useNumberStore, usekesigomuStore, usehintoStore, useWrongStore, useClearflagStore, useMemoStore } from "../stores/level.ts";
+import { usePuzzleGenerator } from "./CreateQuestion";
+const puzzleDiv = ref<HTMLDivElement | null>(null);
 
-    onMounted(() => {
-        puzzleDiv.value?.focus();
-        puzzleDiv.value?.addEventListener('blur', () => {
-            setTimeout(() => {
-                puzzleDiv.value?.focus();
-            }, 0);
-        });
+onMounted(() => {
+    puzzleDiv.value?.focus();
+    puzzleDiv.value?.addEventListener('blur', () => {
+        setTimeout(() => {
+            puzzleDiv.value?.focus();
+        }, 0);
     });
-    
-    usePuzzleGenerator();
-    const cstore = useCreateStore();
-    const cells = ref<{ [key: string]: number | undefined}>({});
-    const memos = ref<Record<string, Set<number>>>({});
-    const readonlyFlags = ref<{ [key: string]: boolean }>({});
-    for (let i = 1; i <= 81; i++) {
-        const key = `cell${i}`;
-        const row = Math.floor((i - 1) / 9);
-        const col = (i - 1) % 9;
-        const value = cstore.create[row][col];
-        readonlyFlags.value[key] = value !== 0;
-        cells.value[key] = value !== 0 ? value : undefined;
-    }
+});
 
-    const flagStore = useClearflagStore()
-    flagStore.flag=false;
-    const filledCount = computed(() => {
+usePuzzleGenerator();
+const cstore = useCreateStore();
+const cells = ref<{ [key: string]: number | undefined }>({});
+const memos = ref<Record<string, Set<number>>>({});
+const readonlyFlags = ref<{ [key: string]: boolean }>({});
+for (let i = 1; i <= 81; i++) {
+    const key = `cell${i}`;
+    const row = Math.floor((i - 1) / 9);
+    const col = (i - 1) % 9;
+    const value = cstore.create[row][col];
+    readonlyFlags.value[key] = value !== 0;
+    cells.value[key] = value !== 0 ? value : undefined;
+}
+
+const flagStore = useClearflagStore()
+flagStore.flag = false;
+const filledCount = computed(() => {
     return Object.entries(cells.value).filter(([key, val]) => {
         if (val === undefined) return false;
         const index = Number(key.replace('cell', '')) - 1;
         const row = Math.floor(index / 9);
         const col = index % 9;
         return val === cstore.answer[row][col];
-        }).length;
-    });
+    }).length;
+});
 
 
-    watch(filledCount, (newVal) => {
-        if (newVal === 81) {
-            flagStore.flag = true;
-        }
-    });
+watch(filledCount, (newVal) => {
+    if (newVal === 81) {
+        flagStore.flag = true;
+    }
+});
 
-    const activeCell = ref<{row:number, col: number}>({ row: 1, col: 1 })
-    const cellINdex = computed(() =>
-        (activeCell.value.row - 1) * 9 + (activeCell.value.col - 1) + 1
-    );
-    const key = computed(() => `cell${cellINdex.value}`);
-    
-    const moveRight = () => {
+const activeCell = ref<{ row: number, col: number }>({ row: 1, col: 1 })
+const cellINdex = computed(() =>
+    (activeCell.value.row - 1) * 9 + (activeCell.value.col - 1) + 1
+);
+const key = computed(() => `cell${cellINdex.value}`);
+
+const moveRight = () => {
     if (activeCell.value.col < 9) activeCell.value.col++
-    }
-    const moveLeft = () => {
+}
+const moveLeft = () => {
     if (activeCell.value.col > 1) activeCell.value.col--
-    }
-    const moveUp = () => {
+}
+const moveUp = () => {
     if (activeCell.value.row > 1) activeCell.value.row--
-    }
-    const moveDown = () => {
+}
+const moveDown = () => {
     if (activeCell.value.row < 9) activeCell.value.row++
-    }
+}
 
-    const onKeyup = (event: KeyboardEvent) => {
-        const key = event.key
-        if (/^[1-9]$/.test(key)){
-            numberStore.setNumber(parseInt(key))
-        }
+const onKeyup = (event: KeyboardEvent) => {
+    const key = event.key
+    if (/^[1-9]$/.test(key)) {
+        numberStore.setNumber(parseInt(key))
     }
+}
 
-    const rowBlock = computed(() => {
-        const start = Math.floor((activeCell.value.row - 1) / 3) * 3 + 1
-        return [start, start + 1, start + 2]
-    })
-    const colBlock = computed(() => {
+const rowBlock = computed(() => {
+    const start = Math.floor((activeCell.value.row - 1) / 3) * 3 + 1
+    return [start, start + 1, start + 2]
+})
+const colBlock = computed(() => {
     const start = Math.floor((activeCell.value.col - 1) / 3) * 3 + 1
     return [start, start + 1, start + 2]
-    })
-    function changeback(row:number,col:number){
-        activeCell.value = { row, col }
-    }
+})
+function changeback(row: number, col: number) {
+    activeCell.value = { row, col }
+}
 
-    const numberStore = useNumberStore();
-    const memo = useMemoStore();
-    const wrongStore = useWrongStore();
-    wrongStore.wrong = 0;
+const numberStore = useNumberStore();
+const memo = useMemoStore();
+const wrongStore = useWrongStore();
+wrongStore.wrong = 0;
 
-    watch(
-        () => numberStore.number,
-        (newnumber) => {
-            if (newnumber === null || flagStore.flag || wrongStore.wrong >= 3) return;
-            if (!readonlyFlags.value[key.value]){
-                if (memo.onoff==="on"){
-                    memoInput(newnumber);
-                }else{
-                    numberInput(newnumber);
-                }
+watch(
+    () => numberStore.number,
+    (newnumber) => {
+        if (newnumber === null || flagStore.flag || wrongStore.wrong >= 3) return;
+        if (!readonlyFlags.value[key.value]) {
+            if (memo.onoff === "on") {
+                memoInput(newnumber);
+            } else {
+                numberInput(newnumber);
             }
-            numberStore.number=null
         }
-    )
-
-    function memoInput(num: number) {
-        if (readonlyFlags.value[key.value]) return;
-        if (!memos.value[key.value]) {
-            memos.value[key.value] = new Set<number>();
-        }
-        if (memos.value[key.value].has(num)) {
-            memos.value[key.value].delete(num);
-        } else {
-            memos.value[key.value].add(num);
-        }
+        numberStore.number = null
     }
+)
 
-    function numberInput(num: number) {
+function memoInput(num: number) {
+    if (readonlyFlags.value[key.value]) return;
+    if (!memos.value[key.value]) {
+        memos.value[key.value] = new Set<number>();
+    }
+    if (memos.value[key.value].has(num)) {
+        memos.value[key.value].delete(num);
+    } else {
+        memos.value[key.value].add(num);
+    }
+}
+
+function numberInput(num: number) {
     if (cells.value[key.value] === num) {
         cells.value[key.value] = undefined;
     } else {
@@ -133,8 +133,7 @@
     }
 }
 
-
-    function removeRelatedMemos(num: number) {
+function removeRelatedMemos(num: number) {
     for (let i = 1; i <= 81; i++) {
         const row = Math.floor((i - 1) / 9) + 1;
         const col = (i - 1) % 9 + 1;
@@ -151,7 +150,7 @@
 }
 
 
-    function getColorClass(row: number, col: number) {
+function getColorClass(row: number, col: number) {
     const index = (row - 1) * 9 + col;
     const key = `cell${index}`;
     const cellValue = cells.value[key];
@@ -162,22 +161,22 @@
 }
 
 
-    const kesiStore = usekesigomuStore()
-    watch(
-        () => kesiStore.kesi,
-        () => {
-            if(flagStore.flag || wrongStore.wrong >= 3) return;
-            if (readonlyFlags.value[key.value] === false){
-                cells.value[key.value] = undefined;
-                memos.value[key.value] = new Set<number>();
-            }
-            kesiStore.kesi = false;
+const kesiStore = usekesigomuStore()
+watch(
+    () => kesiStore.kesi,
+    () => {
+        if (flagStore.flag || wrongStore.wrong >= 3) return;
+        if (readonlyFlags.value[key.value] === false) {
+            cells.value[key.value] = undefined;
+            memos.value[key.value] = new Set<number>();
         }
-    )
+        kesiStore.kesi = false;
+    }
+)
 
-    const hintoStore = usehintoStore()
-    hintoStore.hinto=3
-    watch(
+const hintoStore = usehintoStore()
+hintoStore.hinto = 3
+watch(
     () => hintoStore.hinto,
     (newVal, oldVal) => {
         if (newVal > oldVal) return;
@@ -187,45 +186,33 @@
 
         if (!readonlyFlags.value[cellKey]) {
             cells.value[key.value] = cstore.answer[activeCell.value.row - 1][activeCell.value.col - 1];
-
             readonlyFlags.value[cellKey] = true;
             removeRelatedMemos(cstore.answer[row][col]);
         } else {
             hintoStore.hinto = oldVal;
         }
     }
-    );
+);
 </script>
 
 <template>
-    <div class="Puzzle"
-        ref="puzzleDiv"
-        tabindex="0" 
-        @keyup.right="moveRight" 
-        @keyup.left="moveLeft" 
-        @keyup.up="moveUp" 
-        @keyup.down="moveDown"
-        @keyup="onKeyup"
-    >
+    <div class="Puzzle" ref="puzzleDiv" tabindex="0" @keyup.right="moveRight" @keyup.left="moveLeft" @keyup.up="moveUp"
+        @keyup.down="moveDown" @keyup="onKeyup">
         <table>
             <tr v-for="row in 9" :key="row">
-                <td v-for="col in 9" :key="col" @click="changeback(row,col)" 
-                    :class="[{ 
+                <td v-for="col in 9" :key="col" @click="changeback(row, col)" :class="[{
                     block: rowBlock.includes(row) && colBlock.includes(col),
-                    rank2: activeCell.row === row || activeCell.col === col, 
-                    rank1: activeCell.row === row && activeCell.col === col},
-                    getColorClass(row, col)]"
-                >
+                    rank2: activeCell.row === row || activeCell.col === col,
+                    rank1: activeCell.row === row && activeCell.col === col
+                },
+                getColorClass(row, col)]">
                     <template v-if="cells[`cell${(row - 1) * 9 + col}`] !== undefined">
                         {{ cells[`cell${(row - 1) * 9 + col}`] }}
                     </template>
                     <template v-else>
                         <div class="memo-grid">
-                            <span
-                                v-for="n in 9" :key="n"
-                                class="memo-num"
-                            >
-                                {{ memos[`cell${(row-1)*9+col}`]?.has(n)?n:`` }}
+                            <span v-for="n in 9" :key="n" class="memo-num">
+                                {{ memos[`cell${(row - 1) * 9 + col}`]?.has(n) ? n : `` }}
                             </span>
                         </div>
                     </template>
@@ -236,78 +223,83 @@
 </template>
 
 <style scoped>
+.Puzzle {
+    outline: none;
+}
+
+td {
+    border: 1px solid black;
+    height: 45px;
+    width: 45px;
+    text-align: center;
+    font-size: 150%;
+    position: relative;
+}
+
+td:nth-child(1),
+td:nth-child(4),
+td:nth-child(7) {
+    border-left: 2px solid black;
+}
+
+td:nth-child(9) {
+    border-right: 2px solid black;
+}
+
+tr:nth-child(3n+1) {
+    border-top: 2px solid black;
+}
+
+tr:nth-child(9) td {
+    border-bottom: 2px solid black;
+}
+
+table {
+    border-collapse: collapse;
+}
+
+.block,
+.rank2 {
+    background-color: rgb(219, 236, 252);
+}
+
+.rank1 {
+    background-color: rgb(153, 205, 253);
+}
+
+.correct {
+    color: rgb(1, 1, 157);
+}
+
+.incorrect {
+    color: red;
+}
+
+.memo-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    font-size: 10px;
+    color: gray;
+    padding: 3px;
+}
+
+.memo-num {
+    line-height: 1.2;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+
+@media (max-width:770px) {
     td {
-        border: 1px solid black;
-        height: 45px;
-        width: 45px;
-        text-align: center;
-        font-size: 150%;
-        position: relative;
+        height: 30px;
+        width: 30px;
+        font-size: 100%;
     }
 
-    td:nth-child(1),
-    td:nth-child(4),
-    td:nth-child(7) {
-        border-left: 2px solid black;
+    .memo-grid {
+        font-size: 6px;
     }
-
-    td:nth-child(9){
-        border-right: 2px solid black;
-    }
-
-    tr:nth-child(3n+1){
-        border-top: 2px solid black;
-    }
-
-    tr:nth-child(9) td{
-        border-bottom: 2px solid black;
-    }
-
-    table{
-        border-collapse: collapse;
-    }
-
-    .block, .rank2{
-        background-color: rgb(219, 236, 252);
-    }
-
-    .rank1{
-        background-color: rgb(153, 205, 253);
-    }
-
-    .correct{
-        color: rgb(1, 1, 157);
-    }
-
-    .incorrect{
-        color: red;
-    }
-
-    .memo-grid{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(3, 1fr);
-        font-size: 10px;
-        color: gray;
-        padding: 3px;
-    }
-
-    .memo-num{
-        line-height: 1.2;
-        align-items: center;
-        justify-content: center;
-        display: flex;
-    }
-
-    @media (max-width:770px) {
-        td {
-            height: 30px;
-            width: 30px;
-            font-size: 100%;
-        }
-
-        .memo-grid {
-            font-size: 6px;
-        }
-    }
+}
 </style>
