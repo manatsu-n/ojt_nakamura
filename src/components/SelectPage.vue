3f8b917745ad3b1460d805b243cab8ee248d1c5b
manatsu-n <manatsu.nakamura@kard.co.jp>
2025-06-09 13:22:43 +0900

    fetch変更

--- src/components/SelectPage.vue
+++ src/components/SelectPage.vue
@@ -1,56 +1,54 @@
 <script setup lang="ts">
-  import { useRouter } from 'vue-router';
-  import { useLevelStore, usePuzzlesStore } from '../stores/level.ts';
-  import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useLevelStore, usePuzzlesStore } from '../stores/level.ts';
+import { onMounted } from 'vue';
 
-  const levelStore = useLevelStore();
-  const router = useRouter();
-  const gotogame = (difficulty:string) => {
-    levelStore.setLevel(difficulty);
-    router.push('/game');
-  };
-  
-  const puzzlesStore = usePuzzlesStore(); 
-  onMounted(async () => {
-      try {
-          const res = await fetch('http://localhost:3000/')
-          puzzlesStore.puzzles = await res.json()
-      } catch (error) {
-          console.error('Failed to fetch puzzles:', error)
-      }
-  })
+const levelStore = useLevelStore();
+const router = useRouter();
+const gotogame = (difficulty: string) => {
+  levelStore.setLevel(difficulty);
+  router.push('/game');
+};
+
+const puzzlesStore = usePuzzlesStore();
+onMounted(async () => {
+  try {
+    const res = await fetch('https://backend-frgnd8hpb3cma4b0.japanwest-01.azurewebsites.net/')
+    puzzlesStore.puzzles = await res.json()
+  } catch (error) {
+    console.error('Failed to fetch puzzles:', error)
+  }
+})
 </script>
 
 <template>
   <div>
-  <h1>
-    難易度を選択
-  </h1>
-  <button class="button" @click="gotogame('easy')">easy</button>
-  <button class="button" @click="gotogame('normal')">normal</button>
-  <button class="button" @click="gotogame('hard')">hard</button>
+    <h1>
+      難易度を選択
+    </h1>
+    <button class="button" @click="gotogame('easy')">easy</button>
+    <button class="button" @click="gotogame('normal')">normal</button>
+    <button class="button" @click="gotogame('hard')">hard</button>
   </div>
 </template>
 
 <style scoped>
-  h1{
-    margin: 30px auto;
-    text-align: center;
-  }
+h1 {
+  margin: 30px auto;
+  text-align: center;
+}
 
-  .button{
-    display: block;
-    margin: 50px auto;
-    font-size: 200%;
-    width: 150px;
-    padding: 0;
-    justify-content: center; 
-  }
+.button {
+  display: block;
+  margin: 50px auto;
+  font-size: 200%;
+  width: 150px;
+  padding: 0;
+  justify-content: center;
+}
 
-  #app{
-    display: flex;
-    justify-content: center;
-  }
-
-
+#app {
+  display: flex;
+  justify-content: center;
+}
 </style>
